8186f974a5f692e5466d4a9fea8ebb42537afca8
Pablo <alu0101398327@ull.edu.es>
2023-11-05 11:53:51 +0000

    Creado entorno de trabajo

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <router-link to="/">Inicio</router-link>
+        <router-link to="/login">Iniciar Sesión</router-link>
+        <router-link to="/register">Registro</router-link>
+      </nav>
+    </header>
+    
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      serverMessage: '', // Aquí almacenaremos el mensaje del servidor
+    };
+  },
+  mounted() {
+    // Realiza una solicitud GET al servidor al cargar el componente
+    fetch('http://localhost:3000/') // Asegúrate de ajustar la URL del servidor si es diferente
+      .then((response) => response.text())
+      .then((data) => {
+        this.serverMessage = data; // Almacena el mensaje del servidor en la variable serverMessage
+        console.log(data);
+      });
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>DeskHelper</h1>
+    <p>Esto es una prueba</p>
+    <p>Mensaje del servidor:</p>
+    <p>{{ serverMessage }}</p>
+  </div>
+</template>
