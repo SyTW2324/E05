2149bca133cdb238dde38ca4b4dcc5ccb8adc5d8
Fernando <alu0101413549@ull.edu.es>
2023-11-09 11:00:20 +0000

    inicio sesion presentable pero no funcional

---
+++ client/src/views/Login.vue
@@ -0,0 +1,73 @@
+<script lang="ts">
+import axios from 'axios';
+import validator from 'validator';
+export default {
+  data() {
+    return {
+      email: '',
+      contrasena: '',
+      mensaje: ''
+    };
+  },
+  methods: {
+    async login() {
+      if (!validator.isEmail(this.email)) {
+        alert('El correo electrónico no es válido');
+        return;
+      }
+      const Usuario = {
+        email: this.email,
+        contrasena: this.contrasena
+      };
+
+    }
+
+  }
+}
+</script>
+
+<template>
+  <div class="background">
+  <div class="content-box">
+  <div class="d-flex flex-column align-items-center justify-content-center vh-100">
+    <h1>Iniciar Sesión</h1>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="email">Correo Electrónico</label>
+        <input type="email" id="email" class="form-control" v-model="email">
+      </div>
+      <div class="form-group">
+        <label for="contrasena">Contraseña</label>
+        <input type="password" id="contrasena" class="form-control" v-model="contrasena">
+      </div>
+      <button class="btn btn-primary" type="submit">Iniciar Sesión</button>
+    </form>
+    <p>{{ mensaje }}</p>
+  </div>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  background-image: url('https://bnetcmsus-a.akamaihd.net/cms/gallery/ASW4ACCHGE991428977059569.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  text-align: center;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.content-box {
+  background-color: rgba(255, 255, 255, 0.8); /* Fondo blanco semi-transparente */
+  border: 1px solid #ccc; /* Borde gris */
+  border-radius: 10px; /* Borde redondeado */
+  padding: 20px;
+  text-align: center;
+}
+
+/* Resto de tus estilos */
+</style>
+
